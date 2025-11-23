90286b62e40340ffbe0c3004cb60fad2dca1cec3
Dan Caseley <dan@mobile.dev>
2025-11-23 18:30:00 +0000

    Upgrade jansi to supported jline version

--- maestro-cli/src/main/java/maestro/cli/device/PickDeviceView.kt
+++ maestro-cli/src/main/java/maestro/cli/device/PickDeviceView.kt
@@ -5,7 +5,7 @@ import maestro.cli.util.PrintUtils
 import maestro.device.Device
 import maestro.device.DeviceSpec
 import maestro.device.Platform
-import org.fusesource.jansi.Ansi.ansi
+import org.jline.jansi.Ansi.ansi
 
 object PickDeviceView {
 
